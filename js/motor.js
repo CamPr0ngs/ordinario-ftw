1c621f4edebe93bc3880782f65583167946e9a60
CamPr0ngs <camilacrespozurita@gmail.com>
2026-06-01 12:46:55 -0600

    Agregación de la pantalla Duelistas

--- js/motor.js
+++ js/motor.js
@@ -1,24 +1,24 @@
-// Función para cargar y filtrar los héroes desde el archivo XML
+//Función para cargar y filtrar los héroes desde el archivo XML
 function cargarHeroesPorRol(rolFiltrado, idContenedor) {
-    // 1. Crear el objeto XMLHttpRequest
+    //Crear el objeto XMLHttpRequest
     var xhr = new XMLHttpRequest();
     
-    // 2. Configurar la petición para leer archivo XML
+    //Configurar la petición para leer archivo XML
     xhr.open("GET", "heroes.xml", true);
     
-    // 3. Controlar la respuesta
+    //Controlar la respuesta
     xhr.onreadystatechange = function () {
-        // Validamos que el estado sea 4 (completado) y 200 (OK)
+        //valida que el estado sea 4 (completado) y 200 (OK)
         if (xhr.readyState === 4 && xhr.status === 200) {
             var xmlDoc = xhr.responseXML;
             // obtiene todos los bloques <heroe> del XML
             var heroes = xmlDoc.getElementsByTagName("heroe");
             var contenedor = document.getElementById(idContenedor);
             
-            // Limpiar el contenedor por si tiene texto viejo
+            //Limpiar el contenedor por si tiene texto viejo
             contenedor.innerHTML = "";
             
-            // 4. Recorre los héroes uno por uno
+            //Recorre los héroes uno por uno
             for (var i = 0; i < heroes.length; i++) {
                 var rol = heroes[i].getElementsByTagName("rol")[0].textContent;
                 
@@ -33,7 +33,7 @@ function cargarHeroesPorRol(rolFiltrado, idContenedor) {
                     var descripcion = heroes[i].getElementsByTagName("descripcion")[0].textContent;
                     var imagen = heroes[i].getElementsByTagName("imagen")[0].textContent;
                     
-                    // 5. Crear la tarjeta en html con los datos del personaje
+                    //Crear la tarjeta en html con los datos del personaje
                     var tarjetaHtml = `
                         <div class="tarjeta-heroe">
                             <img src="${imagen}" alt="${nombre}">
@@ -48,13 +48,13 @@ function cargarHeroesPorRol(rolFiltrado, idContenedor) {
                         </div>
                     `;
                     
-                    // insertar la tarjeta en el contenedor de la página
+                    //insertar la tarjeta en el contenedor de la página
                     contenedor.innerHTML += tarjetaHtml;
                 }
             }
         }
     };
     
-    // 4. Enviar la petición de AJAX
+    //Enviar la petición de AJAX
     xhr.send();
 }
